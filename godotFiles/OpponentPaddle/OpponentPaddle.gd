9c6cecf1c77c1e0e8d91b835f1c5162b3c4704e1
bluesentinelsec <bluesentinel@protonmail.com>
2022-07-02 17:37:10 -0400

    first commit

---
+++ godotFiles/OpponentPaddle/OpponentPaddle.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+
+var ball_position
+var speed
+var velocity
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	speed = 100
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Vector2()
+	ball_position = get_parent().get_node("Ball").position
+	
+	if ball_position.x < position.x:
+		velocity = Vector2.LEFT
+		
+	if ball_position.x > position.x:
+		velocity = Vector2.RIGHT
+		
+	position += velocity * speed * delta
+
